959534fbf0ddb53227b769730294e2b0bcd29394
Henry <118751951+HenryFtNonna@users.noreply.github.com>
2025-03-09 19:55:46 +0700

    Commit pack
    base web
    integration firebase

---
+++ reactjs-tinggibadan/src/App.jsx
@@ -0,0 +1,22 @@
+import react from 'react'
+import Info from './components/Info'
+import Compare from './components/Compare'
+import Footer from './components/Footer'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Visualisasi Data Sensor</h1>
+      </header>
+      <Info />
+    </div>
+    
+
+  )
+}
+
+export default App
