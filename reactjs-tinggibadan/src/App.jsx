84fa32ca111e06a81018f949b62061c2baa8cd80
Henry <118751951+HenryFtNonna@users.noreply.github.com>
2025-03-11 19:24:50 +0700

    finishing

--- reactjs-tinggibadan/src/App.jsx
+++ reactjs-tinggibadan/src/App.jsx
@@ -1,18 +1,11 @@
-import react from 'react'
 import Info from './components/Info'
-import Compare from './components/Compare'
-import Footer from './components/Footer'
-
 
 function App() {
-
 
   return (
     <div>
       <Info />
     </div>
-    
-
   )
 }
 
